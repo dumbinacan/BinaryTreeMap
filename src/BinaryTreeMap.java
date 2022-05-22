7d046ce5a580788f1c927e81702a85c29a98721b
DumbInACan <marco@santana.nyc>
2022-05-22 07:32:30 -0400

    organized code and minor fixes

--- src/BinaryTreeMap.java
+++ src/BinaryTreeMap.java
@@ -11,7 +11,6 @@ import java.util.Set;
 
 public class BinaryTreeMap<K, V> implements Map<K, V>{
 	
-/********Data Members******************************/
     private MapNode<K, V> root;
     private int size;
 
@@ -19,9 +18,9 @@ public class BinaryTreeMap<K, V> implements Map<K, V>{
      * Constructs a BinaryTreeMap
      */
     public BinaryTreeMap(){
-        root = null;
-        size = 0;
+        this.clear();
     }
+
     /**
      * Removes all of the mappings from this map
      */
@@ -30,39 +29,86 @@ public class BinaryTreeMap<K, V> implements Map<K, V>{
 	    root = null;
     }
     /**
+     * @return the number of key-value mappings in this map
+     */
+    public int size(){return size;}
+
+    /**
+     * @return true if this map contains no key-value mappings
+     */
+    public boolean isEmpty(){return (root == null);}
+
+    /**
      * @return true if this map contains a mapping for the specified key.
      */
-    public boolean containsKey(Object key) {
-	    return false;
-    }
+    public boolean containsKey(Object key) {return get((K) key) != null;}
+
     /**
      * @return true if this map maps one or more keys to the specified value.
      */
-    public boolean containsValue(Object Value) {
-	    return false;
+    @SuppressWarnings("unchecked")
+    public boolean containsValue(Object value){return recCV(root, (V) value);}
+    private boolean recCV(MapNode<K, V> it, V value){
+        if(it.value() == value){return true;}
+        if(it.left() != null){recCV(it.left(), value);}
+        if(it.right() != null){recCV(it.right(), value);}
+        return false;
     }
-    /****/
+
+    /**
+     * @return V if this map contains a value for the specified key else return null
+     */
     @SuppressWarnings("unchecked")
     public V get(Object k){
         K key = (K) k;
-        if(root == null)
-            return null;
-        if(root.key().equals(key))
-            return root.value();
+        if(root == null){return null;}
         return recGet(root, key);
     }
     private V recGet(MapNode<K, V> it, K key){
-        if(it.left() != null){
-            recGet(it.left(), key);
-        }
-        if(it.right() != null){
-            recGet(it.right(), key);
-        }
-        if(it.key().equals(key)){
-            return it.value();
-        }
+        if(it.key().equals(key)){return it.value();}
+        if(it.left() != null){recGet(it.left(), key);}
+        if(it.right() != null){recGet(it.right(), key);}
         return null;
     }
+
+    // try using get(K) before going into recursion
+    public V put(K key, V value){
+        if(root == null){
+            root = new MapNode<K,V>(key, value);
+            ++size;
+            return null;
+        }
+        else
+            return recPut(root, key, value);
+    }
+    private V recPut(MapNode<K, V> it, K key, V value){
+        V tmp = null;
+        if(it.key() == key){
+            tmp = it.value();
+            it.setValue(value);
+            return tmp;
+        }
+        // whats key  < it.key() mean in Java?
+        if(key < it.key()){
+            if(it.left() != null)
+                recPut(it.left(), key, value);
+            else{
+                it.setLeft(new MapNode<K, V>(key, value));
+                ++size;
+                return null;
+            }
+        }
+        if(key > it.key()){
+            if(it.right() != null)
+                recPut(it.right(), key, value);
+            else{
+                it.setRight(new MapNode<K, V>(key, value));
+                ++size;
+                return null;
+            }
+        }
+    }
+ 
     @SuppressWarnings("unchecked")
     public V remove(Object k){
         V value;
@@ -70,7 +116,7 @@ public class BinaryTreeMap<K, V> implements Map<K, V>{
         K key = (K) k;
         if(root == null)
             return null;
-        if(root.//is leaf and also the key is root... the rest should be the same whether root or not right????
+// TODO if(root.//is leaf and also the key is root... the rest should be the same whether root or not right????
         if(root.key() == key){
             value = root.value();
             if(root.left() != null){// check if right is null after that
@@ -116,63 +162,9 @@ public class BinaryTreeMap<K, V> implements Map<K, V>{
         else
             return prev;
     }
-    public V put(K key, V value){
-        if(root == null){
-            root = new MapNode<K,V>(key, value);
-            ++size;
-            return null;
-        }
-        else
-            return recPut(root, key, value);
-    }
-    private V recPut(MapNode<K, V> it, K key, V value){
-        V tmp = null;
-        if(it.key() == key){
-            tmp = it.value();
-            it.setValue(value);
-            return tmp;
-        }
-        if(key < it.key()){
-            if(it.left() != null)
-                recPut(it.left(), key, value);
-            else{
-                it.setLeft(new MapNode<K, V>(key, value));
-                ++size;
-                return null;
-            }
-        }
-        if(key > it.key()){
-            if(it.right() != null)
-                recPut(it.right(), key, value);
-            else{
-                it.setRight(new MapNode<K, V>(key, value));
-                ++size;
-                return null;
-            }
-        }
-    }
-    public int size(){return size;}
-    public boolean isEmpty(){return (root == null);}
-    @SuppressWarnings("unchecked")
-    public boolean containsValue(Object value){return recCV(root, (V) value);}
-    private boolean recCV(MapNode<K, V> it, V value){
-        if(it.left() != null){
-            recCV(it.left(), value);
-        }
-        if(it.right() != null){
-            recCV(it.right(), value);
-        }
-        if(it.value() == value){
-            return true;
-        }
-        return false;
-    }
-            
-    @SuppressWarnings("unchecked")
-    public boolean containsKey(Object key){return get((K) key) != null;}
+   @SuppressWarnings("unchecked")
     public Set<Map.Entry<K, V>> entrySet(){ return null;}//for now just to get all the methods here and shit
     public Collection<V> values(){return null;}//for now just to get all the methods here and shit
     public Set<K> keySet(){return null;}//for now just to get all the methods here and shit
-    public void clear(){}//for now just to get all the methods here and shit
     public void putAll(Map<? extends K, ? extends V> m){}//for now just to get all the methods here and shit
 }
