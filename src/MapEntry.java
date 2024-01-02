449908e32b8ae4a40d50563d5e98fc21bbcb2f7e
Marco Antonio Santana <marco@santana.nyc>
2024-01-02 00:50:33 -0500

    MapEntry logic done

--- src/MapEntry.java
+++ src/MapEntry.java
@@ -16,8 +16,29 @@ public class MapEntry implements Map.Entry<K,V> {
         return oldval;
     }
 
-public static <K extends Comparable<? super K>,V> Comparator<Map.Entry<K,V>> comparingByKey() {}
-public static <K,V extends Comparable<? super V>> Comparator<Map.Entry<K,V>> comparingByValue() {}
-public static <K,V> Comparator<Map.Entry<K,V>> comparingByKey(Comparator<? super K> cmp) {}
-public static <K,V> Comparator<Map.Entry<K,V>> comparingByValue(Comparator<? super V> cmp) {}
+    public static <K extends Comparable<? super K>,V> Comparator<Map.Entry<K,V>> comparingByKey() {
+
+        // Comparator<MapEntry> cmp = (me1, me2) -> ( me1.getKey() > me2.getKey() ) ? 1 : -1;
+
+        /* Comparator<MapEntry> com = new Comparator<MapEntry>() {
+         *     public int compare(MapEntry me1, MapEntry me2) { 
+         *         if ( me1.getKey() > me2.getKey() ) { return 1; }
+         *         else { return -1; }
+         *     }
+         * };
+         */
+
+        return (me1, me2) -> ( me1.getKey() > me2.getKey() ) ? 1 : -1;
+    }
+
+    public static <K,V extends Comparable<? super V>> Comparator<Map.Entry<K,V>> comparingByValue() {
+        return (me1, me2) -> ( me1.getValue() > me2.getValue() ) ? 1 : -1;
+    }
+// take a comparator for K or V and return a compartor for MapEntry
+    public static <K,V> Comparator<Map.Entry<K,V>> comparingByKey(Comparator<? super K> cmp) {
+        return (me1, me2) -> cmp.compare( me1.getKey(), me2.getKey() ) ? 1 : -1;
+    }
+    public static <K,V> Comparator<Map.Entry<K,V>> comparingByValue(Comparator<? super V> cmp) {
+        return (me1, me2) -> cmp.compare( me1.getValue(), me2.getValue() ) ? 1 : -1;
+    }
 }
